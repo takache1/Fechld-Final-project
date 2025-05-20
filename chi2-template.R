f585171d28f2091b55987b7a5449620a1b46bedf
takache1 <takache1@tcnj.edu>
2025-05-20 18:57:41 -0400

    Add files via upload

---
+++ chi2-template.R
@@ -0,0 +1,80 @@
+
+## Project:  FILL THIS OUT
+# Located:   FILL THIS OUT
+# File Name: FILL THIS OUT
+# Date:      FILL THIS OUT
+# Who:       FILL THIS OUT
+
+
+####################################################################################
+############              Pre-Analysis: settings, packages, and data    ############
+####################################################################################
+
+
+### Settings + Packages
+
+
+### Load data 
+
+
+####################################################################################
+############              PHASE 1: CLEAN DATA FOR ANALYSIS              ############
+####################################################################################
+
+
+## Steps of cleaning variables Clear vars
+# Step 1: Examine variable and coding schema: Table() / summary() 
+# Step 2: Recode (if necessary/warrented): mutate(), ifelse(), etc
+# Step 3: Confirm: table() / summary()
+
+
+
+############                     DEPENDENT VARIABLE                     ############
+############              SUBSTANTIVE VARIABLE NAME HERE                ############
+
+# STEP 1: Examine variable and coding schema 
+
+# STEP 2: Recode if necessary or justify if not neccesary
+
+# STEP 3: Confirm creation (if necessary)
+
+
+############                  INDEPENDENT VARIABLE                    ############
+############              SUBSTANTIVE VARIABLE NAME HERE                ############
+
+# STEP 1: Examine variable and coding schema 
+
+# STEP 2: Recode if necessary or justify if not neccesary
+
+# STEP 3: Confirm creation (if necessary)
+
+
+
+####################################################################################
+############              PHASE 2: CREATE MY DATASET                    ############
+####################################################################################
+
+### STEP 1: Create a list of variables to keep
+
+
+### STEP 2: create a new dataset with only your variables and complete case
+
+
+### STEP 3: Gather Summary Statistics and confirm valid dataset construction
+
+
+
+####################################################################################
+############              PHASE 3: Descriptive Statistics     ############
+####################################################################################
+# TABLE 1: DESCRIPTIVE STATISTICS HERE
+
+
+####################################################################################
+############              PHASE 4: Contingency Table + Chi2                  ############
+####################################################################################
+
+
+# TABLE 2: CONTINGENCY TABLE HERE
+
+
